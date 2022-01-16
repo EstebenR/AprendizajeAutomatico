46f95070f51a49088e21ce621b4edf764c8941a4
Esteban <35503563+EstebenR@users.noreply.github.com>
2022-01-16 17:14:09 +0100

    svm intentado

--- Proyecto/SVM.py
+++ Proyecto/SVM.py
@@ -3,11 +3,11 @@ from pandas.io.parsers import read_csv
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
-categories = ["RP", "EC", "E", "ET", "T", "M", "A"]
+categories = ["E", "ET", "T", "M",]
 
 
-def cargaDatos():
-	table = read_csv('Video_games_esrb_rating.csv', header=0,).to_numpy()
+def cargaDatos(file):
+	table = read_csv(file, header=0,).to_numpy()
 	X = table[:, 1:-1]
 	# Hay 7 tipos de ratings que vienen codificados con un string, asi que los codificamos como ints
 	Y = np.empty([np.shape(table)[0], 7])
@@ -27,7 +27,7 @@ def entrenamiento(X, y, xVal, yVal, trainValues):
 		for j in range(numTrainValues):
 			sigma = trainValues[j]
 			svm = SVC(kernel='rbf', C=cVal, gamma=1/(2 * sigma**2))
-			svm.fit(X, y)
+			svm.fit(X, y.ravel())
 			accuracy = accuracy_score(yVal, svm.predict(xVal))
 			if(accuracy > scoreOpt):
 				svmOpt = svm
@@ -38,35 +38,35 @@ def entrenamiento(X, y, xVal, yVal, trainValues):
 	return svmOpt,cOpt,scoreOpt
 
 def SVM():
-	X, Y = cargaDatos()
+	X, Y = cargaDatos('Video_games_esrb_rating.csv')
+	testX, testY = cargaDatos('test_esrb.csv')
 	m = np.shape(X)[0]
+	
 
-	trainX = X[:int(m*0.6), :]
-	testX = X[int(m*0.6):int(m*0.8), :]
+	trainX = X[:int(m*0.8), :]
 	validateX = X[int(m*0.8):, :]
 
-	trainY = Y[:int(m*0.6), :]
-	testY = Y[int(m*0.6):int(m*0.8), :]
+	trainY = Y[:int(m*0.8), :]
 	validateY = Y[int(m*0.8):, :]
 
 	print("starting training")
 
-	trainValues = np.arange(0,30,1)
+	trainValues = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
 
 	svmOpt = []
 	cOpt = []
 	scoreOpt = []
 	
-	for i in range(7):
+	for i in range(4):
 		_svmOpt, _cOpt, _scoreOpt = entrenamiento(trainX, trainY[:,i], validateX, validateY[:,i], trainValues)
-		svmOpt[i] = _svmOpt
-		cOpt[i] = _cOpt
-		scoreOpt[i] = _scoreOpt
+		svmOpt.append(_svmOpt)
+		cOpt.append(_cOpt)
+		scoreOpt.append(_scoreOpt)
 	
 
 	testScores = 0
-	for i in range(7):
-		testScores += svmOpt.score(testX,testY[:,i])
+	for i in range(4):
+		testScores += svmOpt[i].score(testX,testY[:,i])
 	
 	
 	print(f"Optimum Cs: {cOpt}")
