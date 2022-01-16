c847b639882b2496e95493b806761b59f203cbcc
Esteban <35503563+EstebenR@users.noreply.github.com>
2022-01-16 16:20:32 +0100

    Finito regresion

--- Proyecto/RegresionLogistica.py
+++ Proyecto/RegresionLogistica.py
@@ -52,33 +52,59 @@ def gradienteRegularizado(Theta, X, Y, Lambda):
 	j[0] = grad_0
 	return j
 
-def thetasOptimos(X,y,reg):
-	theta = np.zeros([7,np.shape(X)[1]])
+
+def thetasOptimos(X, y, reg):
+	theta = np.zeros([7, np.shape(X)[1]])
 	for i in range(7):
-		result = fmin_tnc(func=costeRegularizada,x0=theta[i],fprime=gradienteRegularizado,args=(X,y[:,i],reg),messages=0)
-		theta[i]=result[0]
+		result = fmin_tnc(func=costeRegularizada, x0=theta[i], fprime=gradienteRegularizado, args=(
+			X, y[:, i], reg), messages=0)
+		theta[i] = result[0]
 	return theta
 
-def evaluacion(maxIndices,Y):
-	acertados = np.sum(maxIndices==np.where(Y[:]==1))
-	print(f"Porcentaje de valores que han sido correctamente clasificados: {acertados*100/np.shape(maxIndices)[0]}%")
+
+def evaluacion(maxIndices, Y, reg):
+	acertados = np.sum(maxIndices == np.where(Y[:] == 1))
+	print(
+		f"Porcentaje de valores que han sido correctamente clasificados: {acertados*100/np.shape(maxIndices)[0]:.3f}% con lambda = {reg}")
+
 
 def oneVsAll():
-	print(categories)
-	X,Y = cargaDatos()
-	X = np.hstack([np.ones([np.shape(X)[0],1]),X])
+	X, Y = cargaDatos()
+	m = np.shape(X)[0]
+	X = np.hstack([np.ones([m, 1]), X])
 
-	reg = 2
-	theta = thetasOptimos(X,Y,reg)
+	trainX = X[:int(m*0.6), :]
+	testX = X[int(m*0.6):int(m*0.8), :]
+	validateX = X[int(m*0.8):, :]
 
-	results = np.zeros([np.shape(X)[0],np.shape(theta)[0]])
-	for i in range(np.shape(theta)[0]):
-		results[:,i] = sigmoide(np.dot(X,theta[i]))
+	trainY = Y[:int(m*0.6), :]
+	testY = Y[int(m*0.6):int(m*0.8), :]
+	validateY = Y[int(m*0.8):, :]
 
-	maxIndices = np.argmax(results,1)
-	print(np.shape(maxIndices))
-	print(maxIndices)
-	evaluacion(maxIndices,Y)
+	mejorAcertados = 0
+	regOpt = 0
+	thetasOpt = []
+
+	for reg in np.arange(0.1, 3, 0.1):
+		theta = thetasOptimos(trainX, trainY, reg)
+
+		results = np.zeros([np.shape(testX)[0], np.shape(theta)[0]])
+		for i in range(np.shape(theta)[0]):
+			results[:, i] = sigmoide(np.dot(testX, theta[i]))
+
+		maxIndices = np.argmax(results, 1)
+		acertados = np.sum(maxIndices == np.where(testY[:] == 1))
+		if(acertados > mejorAcertados):
+			regOpt = reg
+			thetasOpt = theta
+			mejorAcertados = acertados
+
+	results = np.zeros([np.shape(validateX)[0], np.shape(thetasOpt)[0]])
+	for i in range(np.shape(thetasOpt)[0]):
+		results[:, i] = sigmoide(np.dot(validateX, thetasOpt[i]))
+
+	maxIndices = np.argmax(results, 1)
+	evaluacion(maxIndices, validateY, regOpt)
 
 
 oneVsAll()
